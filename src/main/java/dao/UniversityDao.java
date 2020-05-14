749041e4f0cd6038443c95653cb3940e410e94fe
szarbartosz <szarbartosz@gmail.com>
2020-05-14 14:30:20 +0200

    addMainSubjectToField added

--- src/main/java/dao/UniversityDao.java
+++ src/main/java/dao/UniversityDao.java
@@ -3,9 +3,9 @@ package dao;
 import model.Faculty;
 import model.Field;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import javax.persistence.TypedQuery;
 
 public class UniversityDao {
     private static UniversityDao instance;
@@ -16,14 +16,45 @@ public class UniversityDao {
 
     public void addFaculty(String name) {
         Faculty faculty = new Faculty(name);
-        Session session = sessionFactoryDecorator.openSession();
+        Session session = SessionFactoryDecorator.openSession();
         Transaction transaction = session.beginTransaction();
         session.save(faculty);
         transaction.commit();
         session.close();
     }
 
-    public void addField(int facultyId, String name, int capacity) {
-        Field field = new Field()
+    private Faculty getFacultyById(int facultyId) {
+        Session session = SessionFactoryDecorator.openSession();
+        TypedQuery<Faculty> query = session.createQuery("SELECT F FROM Faculty F WHERE F.facultyId = :facultyId", Faculty.class);
+        query.setParameter("facultyId", facultyId);
+        return query.getSingleResult();
+    }
+
+    public void addField(int facultyId, String name, int capacity) throws Exception {
+        if(capacity <= 0.0) {
+            throw new Exception("Incorrect capacity");
+        }
+        Faculty faculty = getFacultyById(facultyId);
+
+        Field field = new Field(name, capacity);
+        faculty.addField(field);
+        Session session = SessionFactoryDecorator.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(field);
+        transaction.commit();
+        session.close();
+    }
+
+    private Field getFieldById(int fieldId) {
+        Session session = SessionFactoryDecorator.openSession();
+        TypedQuery<Field> query = session.createQuery("SELECT F FROM Field F WHERE F.fieldId = :fieldId", Field.class);
+        query.setParameter("fieldId", fieldId);
+        return query.getSingleResult();
+    }
+
+    public void addMainSubjectToField(int fieldId, String subjectName) {
+        Field field = getFieldById(fieldId);
+
+        field.addMainSubject(subjectName);
     }
 }
